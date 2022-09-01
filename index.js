5b8b8fefe0e941ab857f38571c784df7af7aafc5
Proloycb <proloychacroborty@gmail.com>
2022-09-01 16:16:31 +0600

    cors update

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors({ origin: "https://organic-grocer-warehouse.web.app/" }))
+app.use(
+    cors({
+         origin: "https://organic-grocer-warehouse.web.app" 
+        }))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u6cg8le.mongodb.net/?retryWrites=true&w=majority`;

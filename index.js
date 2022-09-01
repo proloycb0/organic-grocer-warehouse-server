788d5924cc9ee7b72821bd72c8369d1c63ddf88e
Proloycb <proloychacroborty@gmail.com>
2022-09-01 15:46:24 +0600

    cors update

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-app.use(cors());
-app.use(express.json());
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-
+app.use(cors({ origin: "https://organic-grocer-warehouse.web.app/" }))
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u6cg8le.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
